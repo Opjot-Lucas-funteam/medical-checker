111568bd7b9785fc95a47ed42d0f143050403de9
LucasNemeth <l_orion_n@hotmail.com>
2020-04-24 17:40:32 -0400

    did some work, trying to figure out how to save to FB

--- src/App.js
+++ src/App.js
@@ -6,85 +6,28 @@ import List from './List';
 
 
 class App extends Component {
-  constructor(){
-    super();
-
-    this.state={
-      medication:{},
-      
-    }
-
-  }
-
-  componentDidMount(){
-    const dbRef= firebase.database().ref();
-    
-    dbRef.on('value', (response) => {
-      const newState = [];
-      const data = response.val();
-      
-      for (let key in data) {
-        const medicalInfo = {
-          key: key,
-          name: data[key]
-        }
-        // newState.push(data[key]);
-        newState.push(medicalInfo)
-      }
-      this.setState({
-        newMed: newState
-      });
-    });
-  }
-
-  //--------------------Event Handlers--------------------------------//
-  handleChange =(e)=>{
-    this.setState({
-      userInput:e.target.value,
-    })
-  }
-  handleClick =(e, userInput)=>{
-    e.preventDefault();
-    if (this.state.userInput === "" ){
-      alert("please enter info")
-    } else {
-      const dbRef = firebase.database().ref()
-      // dbRef.push(this.state.userInput)
-
-      const medObject={
-        medName:userInput.medName,
-        medDay:userInput.medDay,
-        medTime:userInput.medTime
-      }
-
-      dbRef.push(this.state.userInput);
-
-      this.setState({
-        medName: userInput.medName,
-        medDay: userInput.medDay,
-        medTime: userInput.medTime
-      })
-    }
-    // addMed = (medKey) => {
-    //   const dbRef = firebase.database().ref()
-    //   dbRef.child(medKey).push();
-    // }
-  }
-//----------------------------Render--------------------------------------//
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     medications: [],
+  //     day: [],
+  //     time: []
+  //   }
+  // }
   render() {
     return (
-        <section>
-          <div>
-            <Header />
-          </div>
-          <div>
-            <Form />
-          </div>
-          <div>
-            <List />
-          </div>
-        </section>
-      )
-    }
+      <div>
+        <Form />
+      
+        {/* <ul>
+          
+          {this.state.medications.map((med) => {
+            return <li><p>{med}</p></li>
+          })}
+        </ul> */}
+      </div>
+    )
+  }
 }
-export default App;
+
+  export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 class Form extends Component {
+    constructor() {
+        super();
+        this.state = {
+            medications:[],
+            med: '',
+            day: '',
+            time: ''
+        }
+        const dbRef = firebase.database().ref();
+        dbRef.on('value', (response) => {
+            const newState = [];
+            const data = response.val();
+            for (let key in data) {
+                newState.push(data[key]);
+            }
+            this.setState({
+                medications: newState,
+                day: newState,
+                time: newState
+            });
+        });
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    handleClick(event) {
+        event.preventDefault();
+        const dbRef = firebase.database().ref();
+        dbRef.push(this.med.state.userInput);
+        this.setState({ userInput: "" });
+        // this.time.setState({ userInput: "" });
+        // this.day.setState({ userInput: "" });
+    }
     render() {
         return (
             <div>
                 <form action="submit">
-                    <input type="text" placeholder="Medication" id="medName" name="medName"/>
-                    <input type="text" placeholder="Day" id="medDay" name="medDay"/>
-                    <input type="text" placeholder="Time" id="medTime" name="medTime"/>
-                    <input type="submit" value="submit"/>
+                    <input type="text" placeholder="Medication" id="med" name="med" onChange={this.handleChange} value={this.state.med}/>
+                    <input type="text" placeholder="Day" id="day" name="day" onChange={this.handleChange} value={this.state.day}/>
+                    <input type="text" placeholder="Time" id="time" name="time" onChange={this.handleChange} value={this.state.time}/>
+                    <input type="submit" value="submit" onClick={this.handleClick}/>
                 </form>
+                {this.state.medications.map((med) => {
+                    return <li><p>{med}</p></li>
+                })}
             </div>
         );
     }
