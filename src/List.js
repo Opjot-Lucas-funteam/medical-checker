b6e9f0935c81af66bd9ebf251eeb79c1ba6d52a9
Opjot Singh Nijjar <opjot-singh-nijjar@Opjots-MacBook-Pro.local>
2020-05-20 14:23:18 -0400

    MVP

--- src/List.js
+++ src/List.js
@@ -1,36 +1,60 @@
 import React, { Component } from 'react';
-
+import firebase from 'firebase'
 class List extends Component {
     constructor(){
         super();
         this.state={
-            medications:'',
+            medications:[],
         }
     }
+
+    componentDidMount (){
+        const dbRef = firebase.database().ref();
+        console.log(dbRef)
+        dbRef.on('value', (response) => {
+            
+            const dataFromDb=response.val();
+            console.log(dataFromDb);
+            const newState = [];
+            for (let key in dataFromDb) {
+                newState.push(dataFromDb[key]);
+                console.log(dataFromDb[key])
+            }
+            console.log(newState)
+            this.setState({
+                medications: newState,
+                // medName:this.state.medName,
+                // day: this.state.day,
+                // time: this.state.time
+                
+            });
+        });
+    
+    }
+
     render() {
-        const { key, medicine } = this.props.medProp[0];
-        console.log(key, medicine)
+        // const { key, medicine } = this.props.medProp[0];
+        // console.log(key, medicine)
         return (
 
             <div>
                 <ul>
                      {this.state.medications.map((medicine) => {
-                          {
-                             Object.keys(medicine).map((med, index) => {
-                                 return <li key={index}>{medicine} : {medicine[med]}</li>
-                             },
+                          return(
+                            //  Object.keys(medicine).map((med, index) => {
+                            //      return <li key={index}>{medicine} : {medicine[med]}</li>
+                            //  },
                                 <li>
                                     <h3>
                                         {medicine.medName} : {medicine.day} , {medicine.time}
                                     </h3>
                                 </li>
-                             )
-                         }
+                            //  )
+                          )
                      }
                     )}
                 </ul>
                 {/* )}  */}
-                <p>{this.state.medications}</p>
 
             </div>
         );
